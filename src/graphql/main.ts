0758045265af2e3add076aa4d87aa37277854edc
Daniel Blake <daniel@danielblake.dev>
2025-07-17 13:45:35 -0700

    feat: update gql and markdown (#1)

--- src/graphql/main.ts
+++ src/graphql/main.ts
@@ -1,64 +1,64 @@
 import { Stage } from "../constants/main";
-// const prod = process.env.PRODUCTION
 
 
-export const document = `
-fragment Content on ContentBlock {
-  id
-  heading
-  content {
-    copy
-    image {
-      url
-      width
-      height
+export function document(stage: Stage) {
+  return `
+    fragment Content on ContentBlock {
+      id
+      heading
+      content {
+        copy
+        image {
+          url
+          width
+          height
+        }
+      }
     }
-  }
-}
-{
-  pages(stage: ${Stage.DRAFT}) {
-    title
-    description
-    shero {
-      ...Content
-    }
-    sections {
-      __typename
-      ...Content
-      ... on ContentCollection {
-        id
-        heading
-        type
-        sections {
+    {
+      pages(stage: ${stage}) {
+        title
+        description
+        shero {
           ...Content
         }
-      }
-      ... on ContactBlock {
-        id
-        heading
-        contact {
-          email
-          phone
-          fax
+        sections {
+          __typename
+          ...Content
+          ... on ContentCollection {
+            id
+            heading
+            type
+            sections {
+              ...Content
+            }
+          }
+          ... on ContactBlock {
+            id
+            heading
+            contact {
+              email
+              phone
+              fax
+            }
+            address {
+              street
+              unit
+              city
+              state
+              zipcode
+            }
+          }
         }
-        address {
-          street
-          unit
-          city
-          state
-          zipcode
+        footer {
+          heading
+          resources {
+            id
+            key
+            value
+          }
+          copyright
         }
       }
-    }
-    footer {
-      heading
-      resources {
-        id
-        key
-        value
-      }
-      copyright
-    }
-  }
+    }`;
 }
-`;
