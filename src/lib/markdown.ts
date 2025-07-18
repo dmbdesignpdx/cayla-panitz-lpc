2204a0adb99c429f1adc9d90edfb576a2349524c
Daniel Blake <daniel@danielblake.dev>
2025-07-17 17:37:57 -0700

    feat: update markdown html and links

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -4,18 +4,34 @@
 
 
 import purify from "isomorphic-dompurify";
-import { marked } from "marked";
+import {
+  type RendererObject,
+  type Tokens,
+  marked,
+} from "marked";
 
+
+const renderer: RendererObject = {
+  link({ href, tokens }: Tokens.Link): string {
+    const text = this.parser.parseInline(tokens);
+    const external = href.startsWith("http");
+
+    if (external) return `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+    return `<a href="${href}">${text}</a>`;
+  },
+};
+
+marked.use({ renderer });
 
 export const md = {
   async inline(text: string): Promise<string> {
     const dirty: string = await marked.parseInline(text);
 
-    return purify.sanitize(dirty);
+    return purify.sanitize(dirty, { ADD_ATTR: ["target"] });
   },
   async block(text: string): Promise<string> {
     const dirty: string = await marked.parse(text);
 
-    return purify.sanitize(dirty);
+    return purify.sanitize(dirty, { ADD_ATTR: ["target"] });
   },
 };
